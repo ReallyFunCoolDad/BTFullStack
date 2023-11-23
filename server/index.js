362cb6652d6cae24888ae94cf3a12e89d6679a0f
ReallyFunCoolDad <tylerebarnett@yahoo.com>
2023-11-23 18:49:43 -0500

    get change

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.use(express.static(buildPath));
 // Create a route for sending emails
 app.get("/*", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "../client/dist/index.html"),
+    path.join(__dirname, "../client/dist/", "index.html"),
     function (err) {
       if (err) {
         res.status(500).send(err);
