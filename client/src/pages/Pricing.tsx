287e4e18e4c4304d4797bc8c1c8cd122a4f862dc
ReallyFunCoolDad <tylerebarnett@yahoo.com>
2024-02-10 17:31:06 -0500

    pricing upate

--- client/src/pages/Pricing.tsx
+++ client/src/pages/Pricing.tsx
@@ -7,7 +7,7 @@ const servicesData = [
   {
     title: "Consulting",
     description: "Expert advice and solutions for your IT challenges.",
-    price: "$200 per hour",
+    price: "$100 per hour",
   },
   {
     title: "On-Site Services",
@@ -15,13 +15,17 @@ const servicesData = [
       "We will come to your Home or Business provide IT Services. $100 an hour after the first hour",
     price: "$150 per visit",
   },
+  {
+    title: "Development",
+    description: "We can build websites, web apps, APIs etc...",
+    price: "Varies with project",
+  },
 ];
 
 function Pricing() {
   return (
     <div className="flex flex-col items-center justify-center w-full gap-4 p-2 m-auto mb-6">
       <h1 className="mt-3 text-3xl font-semibold text-center">Pricing</h1>
-      <p className="text-lg text-sky-400">Common Services</p>
       <ul className="w-full">
         {servicesData.map((item, index) => (
           <li
